0f2de746df9eacd9edc98ea56242aecd3c455e55
Ahmed-Hany-saad <ahhasa842@gmail.com>
2026-07-18 19:25:45 +0300

    updates

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = 2000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.sendFile("./view/index.html", { root: __dirname });
 });
 
 
 app.listen(port, () => {
-  console.log(`http://localhost:3000/`);
+  console.log(`http://localhost:${port}/`);
 });
