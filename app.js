69a9e3d55edbf75754acdf1a8cca828eaae5e9c8
Ahmed-Hany-saad <ahhasa842@gmail.com>
2026-07-19 09:44:01 +0300

    updates

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 2000;
+const mongoose = require('mongoose');
 
 app.get('/', (req, res) => {
   res.sendFile("./view/index.html", { root: __dirname });
@@ -9,4 +10,10 @@ app.get('/', (req, res) => {
 
 app.listen(port, () => {
   console.log(`http://localhost:${port}/`);
+});
+
+mongoose.connect('mongodb://127.0.0.1:27017/test').then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
 });
